4485739e4773e41ff24b25f7849d93574083fc5b
Max van der Meulen <98962990+MaxvanderMeulen@users.noreply.github.com>
2023-01-05 22:15:21 +0100

    Update joining.R

--- src/dataprep/joining.R
+++ src/dataprep/joining.R
@@ -6,18 +6,20 @@ library(data.table)
 filtered_tunefind <- fread("../../gen/dataprep/data/filtered_tunefind.csv")
 filtered_playlist <- fread("../../gen/dataprep/data/filtered_playlist.csv")
 
-#inner_join
+#select all the treated songs
 inner_join <- 
   inner_join(filtered_tunefind, filtered_playlist, by = c("song_title" = "name", "song_artist" = "artist_names"), keep = TRUE) %>%
   distinct(song_title, song_artist, .keep_all = TRUE)
 
+#all treated songs with moderating variables
 inner_join_sample <- inner_join %>%
   filter(!is.na(order)) %>% 
   filter(!is.na(duration_seconds_join))
 
-filtered_playlist_sample <- filtered_playlist
-#filtered_playlist_sample <- sample_n(filtered_playlist, 400000)
+#lower sample size, otherwise unable to join/match/transform
+filtered_playlist_sample <- sample_n(filtered_playlist, 400000) 
 
+#join datasets
 regular_join <-
   full_join(inner_join_sample, filtered_playlist_sample) %>%
   unique() %>%
@@ -28,6 +30,7 @@ regular_join$treatment <- ifelse(is.na(regular_join$releasedate_episode), 0, 1)
 #make popularity zero
 regular_join$spotify_popularity <- ifelse(is.na(regular_join$spotify_popularity), 0, regular_join$spotify_popularity)
 
+#clean memory
 file.remove("../../gen/dataprep/data/filtered_tunefind.csv")
 file.remove("../../gen/dataprep/data/filtered_playlist.csv")
 gc()
