1e26c05f7af80509bdf3cb6fc32b76dfed7870c9
Max van der Meulen <98962990+MaxvanderMeulen@users.noreply.github.com>
2023-01-05 16:41:26 +0100

    Update unzip_download.R

--- src/dataprep/unzip_download.R
+++ src/dataprep/unzip_download.R
@@ -14,6 +14,7 @@ install.packages("stargazer", repos= 'https://mirror.lyrahosting.com/CRAN/')
 install.packages("etable", repos= 'https://mirror.lyrahosting.com/CRAN/')
 install.packages("gridExtra", repos= 'https://mirror.lyrahosting.com/CRAN/')
 install.packages("readr", repos= 'https://mirror.lyrahosting.com/CRAN/')
+install.packages("unzip", repos= 'https://mirror.lyrahosting.com/CRAN/')
 
 # activate packages
 library(fixest)
@@ -28,11 +29,13 @@ library(zoo)
 library(stargazer)
 library(etable)
 library(gridExtra)
-
+library(unzip)
 library(readr)
 
-tunefind <- read_csv(unzip("../../data/Archive.zip", "df_tunefind.csv"))
-tracks_playlists <- read_csv(unzip("../../data/Archive.zip", "df_hannes.csv"))
+tunefind <- fread(cmd = 'unzip -p ../../data/Archive.zip df_tunefind.csv')
+tracks_playlists <- fread(cmd = 'unzip -p ../../data/Archive.zip df_hannes.csv')
+#tunefind <- read_csv(unzip("../../data/Archive.zip", "df_tunefind.csv"))
+#tracks_playlists <- read_csv(unzip("../../data/Archive.zip", "df_hannes.csv"))
 
 #Export to csv
 dir.create('../../gen')
