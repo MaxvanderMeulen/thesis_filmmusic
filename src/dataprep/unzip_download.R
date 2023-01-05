6fe9e9f1e1c7e1f70581336b2b1a858f5cff420b
Max van der Meulen <98962990+MaxvanderMeulen@users.noreply.github.com>
2023-01-05 15:43:49 +0100

    Update unzip_download.R

--- src/dataprep/unzip_download.R
+++ src/dataprep/unzip_download.R
@@ -32,11 +32,11 @@ library(gridExtra)
 library(readr)
 
 tunefind <- read_csv(unzip("../../data/Archive.zip", "df_tunefind.csv"))
-tracks_playlists <- read_csv(unzip("../../data/Archive.zip", "df_hannes.csv"))
+#tracks_playlists <- read_csv(unzip("../../data/Archive.zip", "df_hannes.csv"))
 
 #Export to csv
 dir.create('../../gen')
 dir.create('../../gen/dataprep')
 dir.create('../../gen/dataprep/data')
 write.csv(tunefind, "../../gen/dataprep/data/tunefind.csv", row.names = FALSE)
-write.csv(tracks_playlists, "../../gen/dataprep/data/tracks_playlists.csv", row.names = FALSE)
+#write.csv(tracks_playlists, "../../gen/dataprep/data/tracks_playlists.csv", row.names = FALSE)
